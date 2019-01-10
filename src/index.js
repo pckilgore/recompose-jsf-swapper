3a16c6067a07bb34262f4e4dc50a4471edc84512
Patrick Kilgore <git@pck.email>
2019-01-10 15:42:31 -0600

    It works!

--- src/index.js
+++ src/index.js
@@ -1,16 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./styles.css";
+import { compose, withState, withProps, branch } from "recompose";
 
-function App() {
+/*
+  withState("formType", "setFormType"),
+  branch(({ formType }) => formType === "1", withProps({ formData: "1" })),
+  branch(({ formType }) => formType === "2", withProps({ formData: "2" })),
+  branch(({ formType }) => formType === "3", withProps({ formData: "3" })),
+  branch(({ formType }) => formType === "4", withProps({ formData: "4" }))
+*/
+
+const withHOCSwitch = (state, updater, initial, ...branches) => {
+  return compose(
+    withState(state, updater, initial),
+    ...branches.map(({ match, to }) =>
+      branch(({ state: test }) => test === match, to)
+    )
+  );
+};
+
+const App = compose(
+  withHOCSwitch(
+    "formType",
+    "setFormType",
+    "",
+    { match: "1", to: withProps({ formData: "1" }) },
+    { match: "2", to: withProps({ formData: "2" }) },
+    { match: "3", to: withProps({ formData: "3" }) },
+    { match: "4", to: withProps({ formData: "4" }) }
+  )
+)(props => {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1> 🤡 Form </h1>
+      <form onSubmit={() => alert(JSON.stringify(props.formData))}>
+        <select
+          name="formType"
+          onChange={e => props.setFormType(e.target.value)}
+          value={props.formType}
+        >
+          <option value="">---select a type---</option>
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+          <option value="3">Option 3</option>
+          <option value="4">Option 4</option>
+        </select>
+        <div style={{ paddingTop: "16px" }}>
+          <button type="submit">Submit form {props.formData}</button>
+        </div>
+      </form>
     </div>
   );
-}
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
